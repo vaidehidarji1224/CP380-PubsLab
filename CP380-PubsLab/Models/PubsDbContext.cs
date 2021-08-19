51874eb432f63c2a1013c03a8d3a2adff4755fe6
Vaidehi <Vdarji@confederationcollege.ca>
2021-08-18 23:43:53 -0400

    Added database.cs in all field

--- CP380-PubsLab/Models/PubsDbContext.cs
+++ CP380-PubsLab/Models/PubsDbContext.cs
@@ -13,10 +13,24 @@ namespace CP380_PubsLab.Models
             optionsBuilder.UseSqlServer($"Server=(localdb)\\mssqllocaldb;Integrated Security=true;AttachDbFilename={dbpath}");
         }
 
-        // TODO: Add DbSets
+        public DbSet<Employee> Employee { get; set; }   //Employee table
+        public DbSet<Jobs> Jobs { get; set; }
+        public DbSet<Sales> Sales { get; set; }//Job table
+        public DbSet<Titles> Titles { get; set; }       //Titles table
+        public DbSet<Stores> Stores { get; set; }       // Stores table
+      
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Sales>()
+          .HasOne(ho => ho.Store)
+          .Withmany(wm => wm.Store)
+          .HasForeignKey<Stores>(hfk => ho.Id);
+
+            modelBuilder.Entity<Stores>()
+        .HasOne(a => a.Sales)
+        .WithOne(b => b.Storesli)
+        .HasForeignKey<Sales>(b => b.SId);
             // TODO
         }
     }
@@ -24,28 +38,44 @@ namespace CP380_PubsLab.Models
 
     public class Titles
     {
-        // TODO
+       
+        public string title { get; set; }
+       
+        public Sales Sales { get; set; }
+
     }
 
 
     public class Stores
     {
-        // TODO
+        public char stor_id { get; set; }
+        public string namestore { get; set; }
+        public Sales Sales { get; set; }
     }
 
 
     public class Sales
     {
-        // TODO
+        public string stor_id { get; set; }
+        public string  storename { get; set; }
+        public string stor_address { get; set; }
     }
 
     public class Employee
     {
         // TODO
+        public string emp_id { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+
+        public Jobs Job { get; set; }
     }
 
     public class Jobs
     {
         // TODO
+        public Int16 Id { get; set; }
+      
+        public string Desc { get; set; }
     }
 }
